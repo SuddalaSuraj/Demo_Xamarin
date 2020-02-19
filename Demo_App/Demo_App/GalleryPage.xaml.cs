d386c491772a5bada33402299db165df8485f821
Suraj <suddalasurajachary1997@gmail.com>
2020-02-19 18:08:20 +0530

    First Commit

---
+++ Demo_App/Demo_App/GalleryPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo_App
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GalleryPage : ContentPage
+	{
+        public int count = 0;
+        string[] m_imageSources = {
+                "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/arctichare.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/baboon.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/barbara.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/boat.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/cat.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/fruits.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/frymire.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/girl.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/goldhill.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/lena.png",
+                "https://homepages.cae.wisc.edu/~ece533/images/monarch.png"
+        };
+		public GalleryPage ()
+		{
+			InitializeComponent ();
+            setImageSource(0);
+		}
+        private void setImageSource(int _count)
+        {
+            var imageSource = new UriImageSource { Uri = new Uri(m_imageSources[_count])};
+            imageSource.CachingEnabled = false;
+            load_image.Source = imageSource;
+        }
+
+        private void Prev_Clicked(object sender, EventArgs e)
+        {
+            if(count == 0)
+            {
+                count = 12;
+            }
+            count--;
+            setImageSource(count);
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+            if(count == 12)
+            {
+                count = -1;
+            }
+            count++;
+            setImageSource(count);
+        }
+    }
+}
---
+++ Demo_App/Demo_App/SliderPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SliderPage : ContentPage
+	{
+        string[] m_quotes = {"Quote_1","Quote_2","Quote_3","Quote_4","Quote_5","Quote_6","Quote_7","Quote_8","Quote_9","Quote_10","Quote_11","Quote_12"};
+        int m_currentQuote = 0;
+
+        public SliderPage ()
+		{
+			InitializeComponent ();
+            quote_show.Text = m_quotes[m_currentQuote];
+		}
+
+        private void Change_Quote(object sender, EventArgs e)
+        {
+            m_currentQuote = (m_currentQuote + 1) % 12;
+            quote_show.Text = m_quotes[m_currentQuote];
+        }
+    }
+}
